7f9b729cb311bb50b99fb1eeaf640884c6a0192a
Joon Choi <jychoi19@gmail.com>
2015-10-19 02:23:44 +0900

    Show active list

--- app/scripts/Controllers/HomeController.js
+++ app/scripts/Controllers/HomeController.js
@@ -1,5 +1,11 @@
 var app = angular.module('Blocitoff');
 
-app.controller("HomeController", ["$firebaseArray", function($scope, $firebaseArray) {
+app.controller("HomeController", function($scope, $firebaseArray) {
     var ref = new Firebase("https://blocitoff1.firebaseio.com/");
-}]);
+  	$scope.tasks = [
+    	{ name: 'walk the dog'},
+    	{ name: 'buy groceries'},
+    	{ name: 'finish bloc assignment'},
+    	{ name: 'write a song'}
+  	];
+});
