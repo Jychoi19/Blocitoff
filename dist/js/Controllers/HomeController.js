ffe8657e97df286da9b2967e851b2380b19c0878
Joon Choi <jychoi19@gmail.com>
2015-10-19 15:16:57 +0900

    Redesign active tasks

--- dist/js/Controllers/HomeController.js
+++ dist/js/Controllers/HomeController.js
@@ -3,9 +3,9 @@ var app = angular.module('Blocitoff');
 app.controller("HomeController", function($scope, $firebaseArray) {
     var ref = new Firebase("https://blocitoff1.firebaseio.com/");
   	$scope.tasks = [
-    	{ name: 'walk the dog'},
-    	{ name: 'buy groceries'},
-    	{ name: 'finish bloc assignment'},
-    	{ name: 'write a song'}
+    	{ name: 'walk the dog', info: "take the dog for a walk around the block"},
+    	{ name: 'buy groceries', info: "carrots, potatoes, tomatoes, some fruits, etc..."},
+    	{ name: 'finish bloc assignment', info: "try to finish this project"},
+    	{ name: 'write a song', info: "finish the 3-4 songs you're currently working on"}
   	];
 });
