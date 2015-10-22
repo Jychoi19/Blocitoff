35bc8b54a730d6598adb26a556fa2de7df70d91b
Joon Choi <jychoi19@gmail.com>
2015-10-23 01:11:22 +0900

    Add input bar fully functional. Displays task list with description

--- dist/js/Controllers/HomeController.js
+++ dist/js/Controllers/HomeController.js
@@ -1,20 +1,26 @@
 var app = angular.module('Blocitoff');
 
 app.controller("HomeController", ['$scope', '$firebaseArray','$timeout', function($scope, $firebaseArray, $timeout) {
-    var ref = new Firebase("https://blocitoff1.firebaseio.com/");
+    var taskRef = new Firebase("https://blocitoff1.firebaseio.com/");
+    
+    $scope.taskList = $firebaseArray(taskRef);
 
-    $scope.taskList = $firebaseArray(ref);
-    $scope.tasks = [
-    	{ name: 'walk the dog', info: "take the dog for a walk around the block"},
-    	{ name: 'buy groceries', info: "carrots, potatoes, tomatoes, some fruits, etc..."},
-    	{ name: 'finish bloc assignment', info: "try to finish this project"},
-    	{ name: 'write a song', info: "finish the 3-4 songs you're currently working on"}
-	]
+    // Add item to taskList
+    $scope.addTask = function(){
+        var timestamp = new Date().valueOf();
+        $scope.taskList.$add({
+            id: timestamp,
+            task: $scope.taskName,
+            description: $scope.taskDescription,
+        });
+        $scope.taskName = "";
+        $scope.taskDescription = "";
+    };
 
-    $scope.showTask = true;
+    // $scope.showTask = true;
 
-    $timeout(function() {
-        $scope.showTask = false;
-    }, 5000);
+    // $timeout(function() {
+    //     $scope.showTask = false;
+    // }, 5000);
 
 }]);
