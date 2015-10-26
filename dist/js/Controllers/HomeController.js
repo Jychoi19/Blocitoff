59a8af7137f5ed573dbaa69c76ad9322999cb39f
Joon Choi <jychoi19@gmail.com>
2015-10-27 01:38:00 +0900

    Merge conflict corrected. Most up to date version

--- dist/js/Controllers/HomeController.js
+++ dist/js/Controllers/HomeController.js
@@ -22,14 +22,9 @@ app.controller("HomeController", ['$scope', '$firebaseArray','$interval', functi
     $interval(function() {
         for (var i = 0; i < $scope.taskList.length; i++) {
             var interval = new Date() - new Date($scope.taskList[i].timestamp); 
-<<<<<<< HEAD
-            if (interval > 604800000) {
-                $scope.removeTask(i);
-=======
             console.log(interval);
             if (interval > 604800000) {
                 $scope.taskList[i].expired = true;
->>>>>>> hidetasks
             }
         };
     }, 60000);
