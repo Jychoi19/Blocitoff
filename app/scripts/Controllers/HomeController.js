8c803af9698e1652fff5c4534df2acbca8080103
Joon Choi <jychoi19@gmail.com>
2015-10-27 00:22:31 +0900

    Tasks older than 7 days are now hidden instead of being deleted

--- app/scripts/Controllers/HomeController.js
+++ app/scripts/Controllers/HomeController.js
@@ -1,20 +1,31 @@
 var app = angular.module('Blocitoff');
 
-app.controller("HomeController", ['$scope', '$firebaseArray','$timeout', function($scope, $firebaseArray, $timeout) {
-    var ref = new Firebase("https://blocitoff1.firebaseio.com/");
+app.controller("HomeController", ['$scope', '$firebaseArray','$interval', function($scope, $firebaseArray, $interval) {
+    var taskRef = new Firebase("https://blocitoff1.firebaseio.com/");
+    
+    $scope.taskList = $firebaseArray(taskRef);
 
-    $scope.taskList = $firebaseArray(ref);
-    $scope.tasks = [
-    	{ name: 'walk the dog', info: "take the dog for a walk around the block"},
-    	{ name: 'buy groceries', info: "carrots, potatoes, tomatoes, some fruits, etc..."},
-    	{ name: 'finish bloc assignment', info: "try to finish this project"},
-    	{ name: 'write a song', info: "finish the 3-4 songs you're currently working on"}
-	]
+    $scope.addTask = function(){
+        $scope.taskList.$add({
+            timestamp: Firebase.ServerValue.TIMESTAMP,
+            task: $scope.taskName,
+            description: $scope.taskDescription,
+        });
+        $scope.taskName = "";
+        $scope.taskDescription = "";    
+    };
 
-    $scope.showTask = true;
+    $scope.removeTask = function(index){
+        $scope.taskList.$remove(index);
+    }
 
-    $timeout(function() {
-        $scope.showTask = false;
-    }, 5000);
-
+    $interval(function() {
+        for (var i = 0; i < $scope.taskList.length; i++) {
+            var interval = new Date() - new Date($scope.taskList[i].timestamp); 
+            console.log(interval);
+            if (interval > 604800000) {
+                $scope.taskList[i].expired = true;
+            }
+        };
+    }, 60000);
 }]);
