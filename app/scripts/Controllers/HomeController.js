b634372c9f12b2935017ae65f251018d0657d9c8
Joon Choi <jychoi19@gmail.com>
2015-10-29 23:45:54 +0900

    Change look of active/completed tasks

--- app/scripts/Controllers/HomeController.js
+++ app/scripts/Controllers/HomeController.js
@@ -4,7 +4,6 @@ app.controller("HomeController", ['$scope', '$firebaseArray','$interval', functi
     var taskRef = new Firebase("https://blocitoff1.firebaseio.com/");
     
     $scope.title = "Active Tasks";
-
     $scope.taskList = $firebaseArray(taskRef);
 
     $scope.addTask = function(){
@@ -19,15 +18,18 @@ app.controller("HomeController", ['$scope', '$firebaseArray','$interval', functi
 
     $scope.removeTask = function(index){
         $scope.taskList.$remove(index);
-    }
+    };
 
-    $interval(function() {
+    $scope.expiredTask = function() {
         for (var i = 0; i < $scope.taskList.length; i++) {
             var interval = new Date() - new Date($scope.taskList[i].timestamp); 
             console.log(interval);
-            if (interval > 604800000) {
+            if (interval > 604800) {
                 $scope.taskList[i].expired = true;
             }
         };
-    }, 60000);
+    };
+
+    $scope.expiredTask();
+    $interval(function(){ $scope.expiredTask(); }, 60000);
 }]);
