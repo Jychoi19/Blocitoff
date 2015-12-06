7bbbb7222562a9844651f140b088470b27a47945
Joon Choi <jychoi19@gmail.com>
2015-12-06 14:43:32 +0900

    Change code organization

--- app/scripts/Controllers/ActiveController.js
+++ app/scripts/Controllers/ActiveController.js
@@ -1,46 +1,46 @@
 var app = angular.module('Blocitoff');
 
 app.controller("ActiveController", ['$scope', '$firebaseArray','$interval', function($scope, $firebaseArray, $interval) {
-    var taskRef = new Firebase("https://blocitoff1.firebaseio.com/");
-    
-    $scope.title = "Active Tasks";
-    $scope.taskList = $firebaseArray(taskRef);
+	var taskRef = new Firebase("https://blocitoff1.firebaseio.com/");
+	
+	$scope.title = "Active Tasks";
+	$scope.taskList = $firebaseArray(taskRef);
 
-    $scope.addTask = function(){
-        $scope.taskList.$add({
-            timestamp: Firebase.ServerValue.TIMESTAMP,
-            task: $scope.taskName,
-            description: $scope.taskDescription,
-            done: false,
-            critical: false
-        });
-        $scope.taskName = "";
-        $scope.taskDescription = "";    
-    };
+	$scope.addTask = function(){
+		$scope.taskList.$add({
+			timestamp: Firebase.ServerValue.TIMESTAMP,
+			task: $scope.taskName,
+			description: $scope.taskDescription,
+			done: false,
+			critical: false
+		});
+		$scope.taskName = "";
+		$scope.taskDescription = "";    
+	};
 
-    $scope.removeTask = function(index){
-        $scope.taskList.$remove(index);
-    };
+	$scope.removeTask = function(index){
+		$scope.taskList.$remove(index);
+	};
 
-    $scope.saveTask = function(task){
-        $scope.taskList.$save(task);
-    };
+	$scope.saveTask = function(task){
+		$scope.taskList.$save(task);
+	};
 
-    $scope.togglePriority = function(task, event) {
-        event.stopImmediatePropagation();
-        task.critical = !task.critical;
-        $scope.saveTask(task);
-    };
+	$scope.togglePriority = function(task, event) {
+		event.stopImmediatePropagation();
+		task.critical = !task.critical;
+		$scope.saveTask(task);
+	};
 
-    $scope.expiredTask = function() {
-        for (var i = 0; i < $scope.taskList.length; i++) {
-            var interval = new Date() - new Date($scope.taskList[i].timestamp); 
-            if (interval > 604800000) {
-                $scope.taskList[i].expired = true;
-            }
-        };
-    };
+	$scope.expiredTask = function() {
+		for (var i = 0; i < $scope.taskList.length; i++) {
+			var interval = new Date() - new Date($scope.taskList[i].timestamp); 
+			if (interval > 604800000) {
+				$scope.taskList[i].expired = true;
+			}
+		};
+	};
 
-    $scope.taskList.$loaded().then($scope.expiredTask);
-    $interval($scope.expiredTask, 60000);
+	$scope.taskList.$loaded().then($scope.expiredTask);
+	$interval($scope.expiredTask, 60000);
 }]);
