23e3f3739b4c1f561d14abc1c918c45e38eeedba
Joon Choi <jychoi19@gmail.com>
2015-12-06 14:47:32 +0900

    Change more code organization

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,21 @@
 var app = angular.module("Blocitoff", ['ui.router', 'firebase', 'ngMaterial']);
 
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-	$locationProvider.html5Mode({
-		 enabled: true,
-		 requireBase: false
-	 });
-	$urlRouterProvider.otherwise("/active");
-	$stateProvider
-		.state('active', {
-			url: '/active',
-			controller: 'ActiveController',
-			templateUrl: '/templates/active.html'
-		}),
-	$stateProvider
-	.state('complete', {
-		url: '/complete',
-		controller: 'CompleteController',
-		templateUrl: '/templates/complete.html'
-	})    
+  $locationProvider.html5Mode({
+     enabled: true,
+     requireBase: false
+   });
+  $urlRouterProvider.otherwise("/active");
+  $stateProvider
+    .state('active', {
+      url: '/active',
+      controller: 'ActiveController',
+      templateUrl: '/templates/active.html'
+    }),
+  $stateProvider
+  .state('complete', {
+    url: '/complete',
+    controller: 'CompleteController',
+    templateUrl: '/templates/complete.html'
+  })    
  });
